a494ea4e8e307f1f40aa5f8a2e4c8eabf2a47fa2
Desale Fentaw <desale.df@gmail.com>
2020-10-20 11:54:12 +0200

    stop button is added

--- UI/ServerUI.py
+++ UI/ServerUI.py
@@ -1,6 +1,6 @@
 import sys
 
-# import cv2
+import cv2
 from PyQt5 import QtWidgets, QtCore, QtGui
 from PyQt5.QtCore import QThread, pyqtSignal
 from PyQt5.QtGui import QPixmap, QImage
@@ -23,6 +23,7 @@ class App(QMainWindow):
         self.filePath = None
         self.image_label = QLabel('Video to be shown where', self)
         self.start_video_button = QtWidgets.QPushButton('Start', self)
+        self.stop_video_button = QtWidgets.QPushButton('Stop', self)
         self.bold_font = QtGui.QFont("Times", 10, QtGui.QFont.Bold)
 
         # menu bar for file
@@ -55,17 +56,20 @@ class App(QMainWindow):
     # to group components on the main window
     def group_components(self):
         # create grid layout to group components
-        girdLayout = QGridLayout()
+        gridLayout = QGridLayout()
         buttonGroupBox = QGroupBox("Properties")
         buttonGroupBox.setMaximumHeight(500)
 
         # setup buttons to control video
-        self.start_video_button.clicked.connect(self.start_video)
+        self.start_video_button.clicked.connect(self.stop_video)
         self.start_video_button.setFont(self.bold_font)
+        self.stop_video_button.clicked.connect(self.stop_video)
+        self.stop_video_button.setFont(self.bold_font)
 
         # add component to the gridLayout here
-        girdLayout.addWidget(self.start_video_button, 4, 0)
-        buttonGroupBox.setLayout(girdLayout)
+        gridLayout.addWidget(self.start_video_button, 1, 0)
+        gridLayout.addWidget(self.stop_video_button, 1, 1)
+        buttonGroupBox.setLayout(gridLayout)
         return buttonGroupBox
 
     # arrange the video and the other components in box layout
@@ -83,6 +87,10 @@ class App(QMainWindow):
 
     # start display camera feed on the image label
     def start_video(self):
+        pass
+
+    # stop displaying camera feed to the image label
+    def stop_video(self):
         pass
 
     # to open file manager
