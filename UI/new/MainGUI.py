7bc4d84ca0abb5c29c2dc0f709b522db6ea6b796
Aregawi Halefom <aregawihalefom90@gmail.com>
2020-11-03 18:05:08 +0100

    UI arranged

---
+++ UI/new/MainGUI.py
@@ -0,0 +1,312 @@
+import sys
+
+import cv2
+from PyQt5 import QtWidgets, QtCore, QtGui
+from PyQt5.QtCore import QThread, pyqtSignal, QTimer
+from PyQt5.QtGui import QPixmap, QImage, QIcon, QCursor
+from PyQt5.QtWidgets import QWidget, QLabel, QApplication, QMainWindow, QVBoxLayout, QSlider, QGridLayout, QGroupBox
+
+
+class App(QMainWindow):
+
+    def __init__(self):
+        super(App, self).__init__()
+
+        # set main window attributes
+        self.title = 'PROJECTION AR'
+        self.left = 300
+        self.top = 100
+        self.width = 1024
+        self.height = 900
+        self.bold_font = QtGui.QFont("Times", 10, QtGui.QFont.Bold)
+        self.video_started = False
+
+        # setting cursor
+
+        # extra parameters
+        self.timer = QTimer()
+        self.cap = None
+        self.filePath = None
+        self.cursor = None
+
+        # 1. components of the UI
+        self.createUIComponents()
+
+        # 2. menu bar for file
+        self.createMenu()
+
+        # 3. Build UI
+        self.buildUI()
+
+    def buildUI(self):
+        """
+
+        """
+        # window location and title
+        self.setWindowTitle(self.title)
+        self.setGeometry(self.left, self.top, self.width, self.height)
+
+        # group the UI compoents
+        buttonGroup = self.groupcomponents()
+
+        # set timer timeout callback function
+        self.timer.timeout.connect(self.displayImage)
+
+        #  final layout.. all compoents together
+        self.finalUi(buttonGroup)
+
+        # show the UI
+        self.show()
+
+    def createUIComponents(self):
+        """
+
+        """
+        # Main video label
+        self.videoLabel = QLabel('Video feed not avaliable , Start video by clicking on the start button on the top '
+                                 'left of the tool list', self)
+        self.videoLabel.setFont(self.bold_font)
+
+        # Start video button
+        self.startVideoButton = QtWidgets.QPushButton(" Strat Video ", self)
+        self.startVideoButton.setIcon(QIcon("icons/icons8-start-50.png"))
+        self.startVideoButton.clicked.connect(self.startVideo)
+
+        # Pencil tool
+        self.pencilButton = QtWidgets.QPushButton(" Draw ", self)
+        self.pencilButton.setIcon(QIcon("icons/icons8-pencil-50.png"))
+        self.pencilButton.clicked.connect(self.drawUsingPencil)
+
+        # rectangle tool
+        self.rectangleButton = QtWidgets.QPushButton(" Select ", self)
+        self.rectangleButton.setIcon(QIcon("icons/icons8-rectangle-50.png"))
+
+        # lasso tool
+        self.lassoButton = QtWidgets.QPushButton(" Lasso ", self)
+        self.lassoButton.setIcon(QIcon("icons/icons8-lasso-tool-48.png"))
+
+        # Stop Video button
+        self.stopVideoButton = QtWidgets.QPushButton(" Pause ", self)
+        self.stopVideoButton.setIcon(QIcon("icons/icons8-stop-squared-50.png"))
+        self.stopVideoButton.clicked.connect(self.stopVideo)
+
+        self.exitButton = QtWidgets.QPushButton(" exit ", self)
+        self.exitButton.setIcon(QIcon("icons/icons8-exit-50.png"))
+        self.exitButton.clicked.connect(self.exitApp)
+
+        self.change_button_status()
+
+    def createMenu(self):
+
+        """
+
+        """
+        self.fileMenuBar = self.menuBar().addMenu('File')
+        self.editMenuBar = self.menuBar().addMenu('Edit')
+
+        self.actionExit = self.fileMenuBar.addAction('Exit')
+        self.actionOpen = self.fileMenuBar.addAction('Open File')
+
+        # create menu bar to put some menu Options
+        self.actionOpen.setShortcut('Ctrl+O')
+        self.actionOpen.triggered.connect(self.openFileNamesDialog)
+
+        # add exit option to exit the program(keyboard shortcut ctrl+q)
+        self.actionExit.setShortcut('Ctrl+Q')
+        self.actionExit.triggered.connect(self.exitApp)
+
+    def groupcomponents(self):
+
+        """
+
+        """
+        # create grid layout to group components
+        gridLayout = QGridLayout()
+
+        toolsButtonGroupBox = QGroupBox("Tools")
+        toolsButtonGroupBox.setToolTip("Tool Box")
+        # buttonGroupBox.setMaximumHeight(500)
+
+        # add component to the gridLayout here
+        gridLayout.addWidget(self.startVideoButton, 1, 0)
+        gridLayout.addWidget(self.stopVideoButton, 1, 1)
+        # toolsButtonGroupBox.setLayout(gridLayout)
+
+        # vertical layout
+        vLayout = QVBoxLayout()
+        vLayout.setSpacing(0)
+        vLayout.setContentsMargins(0, 0, 0, 0)
+        vLayout.addWidget(self.startVideoButton)
+        vLayout.addWidget(self.pencilButton)
+        vLayout.addWidget(self.rectangleButton)
+        vLayout.addWidget(self.lassoButton)
+        vLayout.addWidget(self.stopVideoButton)
+        vLayout.addWidget(self.exitButton)
+
+        # set the layout to the button group
+        toolsButtonGroupBox.setLayout(vLayout)
+
+        return toolsButtonGroupBox
+
+    def finalUi(self, buttonGroup):
+        """
+
+        """
+        gridLayout = QGridLayout()
+
+        # add widgets to the layout
+        gridLayout.addWidget(buttonGroup, 0, 0)
+        gridLayout.addWidget(self.videoLabel, 0, 1, 1, 2)
+
+        # widget for the general layout
+        wid = QtWidgets.QWidget(self)
+        self.setCentralWidget(wid)
+        wid.setLayout(gridLayout)
+
+    def startVideo(self):
+        """
+
+        """
+        try:
+            # if timer is stopped
+            if not self.timer.isActive():
+                # create video capture  and start timer
+                self.cap = cv2.VideoCapture(0)
+                self.videoLabel.setText("Connecting to camera")
+                self.video_started = True
+                self.change_button_status()
+                self.timer.start(10)
+
+
+        except Exception as ex:
+            print(ex)
+
+    def stopVideo(self):
+        """
+
+        """
+        # if the camera is alraedy started close it
+        if self.timer.isActive():
+            # close camera
+            self.cap.release()
+            self.image_label.setText("Camera is closed")
+
+        else:
+            self.image_label.setText('Camera is not started')
+
+    def displayImage(self):
+        """
+
+        """
+        try:
+            # read form camera
+            ret, image = self.cap.read()
+            image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+            # image = cv2.Canny(image, 70, 120)
+
+            # get image info
+            height, width, channel = image.shape
+            step = channel * width
+            # create QImage from imageQImage::Format_Grayscale8
+            qImg = QImage(image.data, width, height, step, QImage.Format_RGB888)
+
+            # show image in img_label
+            self.videoLabel.setPixmap(QPixmap.fromImage(qImg))
+            # call back for drawing
+            cv2.setMouseCallback("Mouse moves", draw_shape)
+
+        except Exception as ex:
+            print(ex)
+
+    def drawUsingPencil(self):
+
+        # change the cursor to pencil
+        CURSOR_NEW = QtGui.QCursor(QtGui.QPixmap('cursors/icons8-pencil-28.png'))
+        self.startVideoButton.setEnabled(False)
+        self.setCursor(CURSOR_NEW)
+        pass
+
+    def openFileNamesDialog(self):
+        """
+
+        """
+        options = QtWidgets.QFileDialog.Options()
+        options |= QtWidgets.QFileDialog.DontUseNativeDialog
+        files, _ = QtWidgets.QFileDialog.getOpenFileNames(
+            self, "QtWidgets.QFileDialog.getOpenFileNames()",
+            "", "Dicom Files (*.*)", options=options)
+        if files:
+            self.filePath = files[0]
+            # go ahead and read the file if necessary
+
+    def exitApp(self):
+        """
+
+        """
+        sys.exit(0)
+
+    def change_button_status(self):
+        """
+
+        """
+        if not self.video_started:
+            self.stopVideoButton.setEnabled(False)
+            self.pencilButton.setEnabled(False)
+            self.rectangleButton.setEnabled(False)
+            self.lassoButton.setEnabled(False)
+            self.startVideoButton.setEnabled(True)
+
+        else:
+            self.startVideoButton.setEnabled(False)
+            self.stopVideoButton.setEnabled(True)
+            self.pencilButton.setEnabled(True)
+            self.lassoButton.setEnabled(True)
+            self.rectangleButton.setEnabled(True)
+
+
+class Events:
+
+    def __init__(self):
+        pass
+
+
+draw = xi = yi = new_line = xi_backup = yi_backup = 0
+
+
+# mouse callback function
+def draw_shape(event, x, y, flags, param):
+    global draw, xi, yi, new_line, xi_backup, yi_backup
+    if event == cv2.EVENT_LBUTTONDOWN:
+        draw = True
+        xi.append(x)
+        yi.append(y)
+    # new_line += 1
+
+    elif event == cv2.EVENT_MOUSEMOVE:
+        if draw:
+            xi.append(x)
+            yi.append(y)
+
+    elif event == cv2.EVENT_LBUTTONUP:
+        xi.append(-4)
+        yi.append(-4)
+        new_line += 1
+        draw = False
+
+    elif event == cv2.EVENT_MOUSEWHEEL:
+        xi_backup, yi_backup = xi, yi
+        print(xi_backup)
+        print("Cleared Mouse event")
+        xi.clear()
+        yi.clear()
+        new_line = 0
+    elif event == cv2.EVENT_RBUTTONDBLCLK:
+        print("Button double clicked")
+        xi, yi = xi_backup, yi_backup
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = App()
+    ex.show()
+    sys.exit(app.exec_())
---
+++ src/prevous/AR-UBx-1.py
@@ -0,0 +1,245 @@
+import argparse
+
+import cv2
+import numpy as np
+import math
+import os
+import argparse
+from objLoader import *
+
+MIN_MATCHES = 10
+
+
+class AR():
+    """" Extract detail information  for the AR implementation"""
+
+    def __init__(self, input_image, scale):
+
+        self.scale = scale  # scale for the 3D object size
+        self.reference_image = input_image  # reference image
+        self.H = None  # Homography
+        self.auto = False
+
+        # matrix of camera parameters (made up but works quite well for me)
+        self.A = np.array([[800, 0, 320], [0, 800, 240], [0, 0, 1]])
+
+        # 3D object to be displayed
+        self.object3D = OBJ('../assets/models/fox.obj', swapyz=True)
+
+        # feature extractor and descriptor
+        self.sift = cv2.xfeatures2d.SIFT_create()
+
+        # start the process
+        self.kp_model, self.des_model, self.model_image = self.intialize_refence_image()
+
+        # Perform the main application task ( grab_video , extract features , descriptors and create homograpy)
+        # then project object
+        self.start_main_task()
+
+    def intialize_refence_image(self):
+
+        # Read the model image
+        img1 = cv2.resize(cv2.imread(self.reference_image, 0), (400, 600))  # query Image
+
+        # Initiate SIFT detector
+        # find the keypoints and descriptors with SIFT
+        kp1, des1 = self.sift.detectAndCompute(img1, None)
+
+        return kp1, des1, img1
+
+    def start_main_task(self):
+
+        cap = cv2.VideoCapture(0)
+
+        cap.set(cv2.CAP_PROP_FRAME_WIDTH, 800)
+        cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 800)
+
+        count = 0
+        while True:
+            # get the image
+            ret, frame = cap.read()
+            if not ret:
+                print("Unable to capture video")
+                return
+            if not count and self.auto:
+                self.reference_image = frame
+                self.kp_model, self.des_model, self.model_image = self.intialize_refence_image()
+
+                # detect and describe features in the feature
+            kp_f, des_f = self.sift.detectAndCompute(frame, None)
+
+            # matching points between the model and the current frame
+            good_matches, all_matches = self.compute_matching_points(kp_f, des_f)
+
+            if len(all_matches) > MIN_MATCHES:
+                # determine homography
+                self.H = self.calculate_homography(good_matches, kp_f)
+
+                # create boundary around the frame
+                bounded_image = self.draw_boundary(frame)
+
+                # bounded image
+                bounded_image = frame
+                # show the 3D OBJECT
+                if self.H is not None:
+                    try:
+                        # obtain 3D projection matrix from H matrix and camera parameters
+                        projection = self.projection_matrix()
+                        # project cube or model
+                        frame = self.show_3d(bounded_image, self.object3D, projection, self.model_image, False)
+                    except Exception as e:
+                        print(e)
+                else:
+                    print("Homography was not created")
+
+                # finally show the final result
+                # path = "results"
+                # if not os.path.exists(path):
+                #     os.makedirs(path)
+                # file_name = os.path.join(path, str(count)+".png")
+                # cv2.imwrite(file_name,frame)
+                cv2.imshow('frame', frame)
+                count += 1
+                if cv2.waitKey(1) == 27 or 0xFF == ord('q'):
+                    break
+            else:
+                print("No enough points")
+                exit()
+
+    def draw_boundary(self, frame):
+        h, w = self.model_image.shape[0], self.model_image.shape[1]
+        pts = np.float32([[0, 0], [0, h - 1], [w - 1, h - 1], [w - 1, 0]]).reshape(-1, 1, 2)
+        dst = cv2.perspectiveTransform(pts, self.H)
+        frame = cv2.polylines(frame, [np.int32(dst)], True, (255, 225, 0), 3, cv2.LINE_AA)
+
+        return frame
+
+    def compute_matching_points(self, kp_f, des_f):
+
+        # match descriptor with model
+        # FLANN MATCHER
+
+        FLANN_INDEX_KDTREE = 0
+
+        index_params = dict(algorithm=FLANN_INDEX_KDTREE, trees=5)
+        search_params = dict(checks=50)
+
+        flann = cv2.FlannBasedMatcher(index_params, search_params)
+        matches = flann.knnMatch(self.des_model, des_f, k=2)
+
+        # store all the good matches as per Lowe's ratio test.
+        good = []
+        for m, n in matches:
+            if m.distance < 0.75 * n.distance:
+                good.append(m)
+
+        return good, matches
+
+    def calculate_homography(self, good, kp_f):
+
+        try:
+
+            # make the points into array.. for RANSAC
+            src_pts = np.float32([self.kp_model[m.queryIdx].pt for m in good]).reshape(-1, 1, 2)
+            dst_pts = np.float32([kp_f[m.trainIdx].pt for m in good]).reshape(-1, 1, 2)
+
+            # find H
+            H, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC, 5.0)
+            matchesMask = mask.ravel().tolist()
+
+            h, w = self.model_image.shape[0], self.model_image.shape[1]
+            pts = np.float32([[0, 10], [0, h - 60], [w - 1, h - 60], [w - 1, 10]]).reshape(-1, 1, 2)
+            dst = cv2.perspectiveTransform(pts, H)
+
+        except Exception as error:
+            print(error)
+            raise RuntimeError
+
+        # homography
+        return H
+
+    # create projection matrix
+    def projection_matrix(self):
+        """
+          From the camera calibration matrix and the estimated H
+          compute the 3D projection matrix
+          """
+        # Compute rotation along the x and y axis as well as the translation
+        H = self.H * (-1)
+        rot_and_transl = np.dot(np.linalg.inv(self.A), H)
+        col_1 = rot_and_transl[:, 0]
+        col_2 = rot_and_transl[:, 1]
+        col_3 = rot_and_transl[:, 2]
+
+        # normalise vectors
+        l = math.sqrt(np.linalg.norm(col_1, 2) * np.linalg.norm(col_2, 2))
+        rot_1 = col_1 / l
+        rot_2 = col_2 / l
+        translation = col_3 / l
+
+        # compute the orthonormal basis
+        c = rot_1 + rot_2
+        p = np.cross(rot_1, rot_2)
+        d = np.cross(c, p)
+        rot_1 = np.dot(c / np.linalg.norm(c, 2) + d / np.linalg.norm(d, 2), 1 / math.sqrt(2))
+        rot_2 = np.dot(c / np.linalg.norm(c, 2) - d / np.linalg.norm(d, 2), 1 / math.sqrt(2))
+        rot_3 = np.cross(rot_1, rot_2)
+        # finally, compute the 3D projection matrix from the model to the current frame
+        projection = np.stack((rot_1, rot_2, rot_3, translation)).T
+        return np.dot(self.A, projection)
+
+    def show_3d(self, img, obj, projection, model, color=False):
+        """
+        Render a loaded obj model into the current video frame
+        """
+        vertices = obj.vertices
+        scale_matrix = np.eye(3) * self.scale
+        h, w = model.shape
+
+        for face in obj.faces:
+            face_vertices = face[0]
+            points = np.array([vertices[vertex - 1] for vertex in face_vertices])
+            points = np.dot(points, scale_matrix)
+            # render model in the middle of the reference surface. To do so,
+            # model points must be displaced
+            points = np.array([[p[0] + w / 2, p[1] + h / 2, p[2]] for p in points])
+            dst = cv2.perspectiveTransform(points.reshape(-1, 1, 3), projection)
+            imgpts = np.int32(dst)
+            if color is False:
+                cv2.fillConvexPoly(img, imgpts, (137, 27, 211))
+            else:
+                color = self.hex_to_rgb(face[-1])
+                color = color[::-1]  # reverse
+                cv2.fillConvexPoly(img, imgpts, color)
+
+        return img
+
+    def hex_to_rgb(hex_color):
+        """
+           Helper function to convert hex strings to RGB
+           """
+        hex_color = hex_color.lstrip('#')
+        h_len = len(hex_color)
+        return tuple(int(hex_color[i:i + h_len // 3], 16) for i in range(0, h_len, h_len // 3))
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="This program projects 3D object on top of reference image in a "
+                                                 "video sequence")
+    parser.add_argument("--input_dir",
+                        type=str,
+                        help="path to reference image.")
+    parser.add_argument("--scale",
+                        type=int,
+                        help="Scale of the 3d object to be viewed , Recommended  values between 0.2 - 0.5")
+    # Execute the parser to create Namespace object containing the above
+    # Properties
+    args = parser.parse_args()
+    input_path = args.input_dir
+    scale = args.scale
+
+    if input_path is None:
+        input_path = "../assets/images/1.jpg"
+    if not scale:
+        scale = 3
+    ar = AR(input_path, scale)
